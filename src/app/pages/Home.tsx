e3f323a14b23aa615b12f2103aa7c8e03ffc04e5
Joel Gustafson <joelgustafson@protonmail.com>
2025-08-19 23:28:13 -0400

    Update Home.tsx

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,13 +1,29 @@
 import { RequestInfo } from "rwsdk/worker";
 
-export function Home({ ctx, request }: RequestInfo) {
+import { client } from "@/app/oauth/oauth-client";
+
+export async function Home({ ctx, request }: RequestInfo) {
+	let user: string | null = null;
+	if (ctx.session !== null) {
+		try {
+			const identity = await client.identityResolver.resolve(ctx.session.did);
+			if (identity.handle === "handle.invalid") {
+				user = identity.did;
+			} else {
+				user = identity.handle;
+			}
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
 	return (
 		<div className="content">
 			<p>hello world</p>
 			<p>
 				<a href="/directory">directory</a>
 			</p>
-			{ctx.session && <p>you are logged in as {ctx.session.did}</p>}
+			{user && <p>you are logged in as {user}</p>}
 		</div>
 	);
 }
