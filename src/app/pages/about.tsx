ce6db80c102eeedd98980e23da908ffce8dab161
Joel Gustafson <joeltg@alum.mit.edu>
2026-02-04 20:44:44 -0500

    Update about.tsx

--- src/app/pages/about.tsx
+++ src/app/pages/about.tsx
@@ -13,7 +13,7 @@ export async function About({}: RequestInfo) {
 			<div className="whitespace-pre overflow-clip tracking-widest my-12 select-none">{blizzardHeader}</div>
 			<div className="content mt-16 mb-12 max-w-md">
 				<p>
-					Snowpost is built on <a href="https://atproto.com/">ATProto</a>, the same protocol that powers BlueSky.
+					Snowpost is built on <a href="https://atproto.com/">ATProto</a>, the same protocol that powers Bluesky.
 				</p>
 				<p>
 					Your posts are stored as raw markdown on your PDS. This means that you always retain access and ownership,
