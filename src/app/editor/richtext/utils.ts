891c57338581836c860458c67eb340b764713d0a
Joel Gustafson <joeltg@alum.mit.edu>
2026-02-07 12:03:51 -0500

    delete richtext editor

--- src/app/editor/richtext/utils.ts
+++
@@ -1,9 +0,0 @@
-// export function signalInvalidType(value: never): never {
-// 	throw new Error(`invalid type: ${value}`);
-// }
-
-// export function assert(condition: any, message?: string): void {
-// 	if (!condition) {
-// 		throw new Error(message ?? "assert failed");
-// 	}
-// }
